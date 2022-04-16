157ed073995471bd680599550899922446072d81
Joaquin Pujols <54540355+jpujols@users.noreply.github.com>
2022-04-16 23:42:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+'''
+if /elif/else
+
+if condition1:
+  do A
+elif condition2:
+  do B
+else:
+  do C
+
+****
+Multiple conditions needs to be check
+if condition1:
+  do A
+if condition2:
+  do B
+if condition3:
+  do C
+
+'''
+print("Welcome to 9Flags v2")
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
+
+if height >= 120:
+  print("You can ride the rollercoaster!")
+  age = int(input("What is your age? "))
+  if age < 12:
+    bill = 5
+    print("Child tickets are $5.")
+  elif age <= 18:
+    bill = 7
+    print("Youth tickets are $7.")
+  else:
+    bill = 12
+    print("Adult tickets are $12.")
+  
+  wants_photo = input("Do you want a photo taken? Y or N. ")
+  if wants_photo == "Y":
+    bill += 3
+  
+  print(f"Your final bill is ${bill}")
+
+else:
+  print("Sorry, you have to grow taller before you can ride.")
